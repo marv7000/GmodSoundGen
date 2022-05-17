8b2aeb8beb0f2e0f9941bd0e3f436c1ba27bf984
Marvin Friedrich <marv7000@web.de>
2022-05-17 21:20:08 +0200

    hello

---
+++ Interop.cs
@@ -0,0 +1,43 @@
+﻿namespace GmodSoundGen;
+
+public class Interop
+{
+    public static void WriteLog(string message, MessageType type)
+    {
+        switch (type)
+        {
+            case MessageType.Error:
+                Console.BackgroundColor = ConsoleColor.DarkRed;
+                Console.Write("[ERROR]\t");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine(message);
+                break;
+            case MessageType.Warning:
+                Console.BackgroundColor = ConsoleColor.DarkYellow;
+                Console.Write("[WARN]\t");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine(message);
+                break;
+            case MessageType.Info:
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.Write("[INFO]\t");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine(message);
+                break;
+            case MessageType.Debug:
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.Write("[DEBUG]\t");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine(message);
+                break;
+        }
+    }
+
+    public enum MessageType
+    {
+        Error,
+        Warning,
+        Info,
+        Debug
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿/// MIT License
+/// Program by marv7000
+
+namespace GmodSoundGen;
+
+public class SoundGen
+{
+    static readonly int Version = 1;
+    
+    public static void Main(string[] args)
+    {
+        Interop.WriteLog($"GmodSoundGen v{Version}", Interop.MessageType.Info);
+        try
+        {
+            Interop.WriteLog($"Reading file:\t{args[0]}", Interop.MessageType.Info);
+            var file = ReadCSV(args[0]);
+        }
+        catch(Exception e)
+        {
+            Interop.WriteLog(e.Message, Interop.MessageType.Error);
+            return;
+        }
+    }
+
+    /// <summary>
+    /// Reads a CSV spreadsheet and returns a 2D string array.
+    /// </summary>
+    /// <param name="fileName">File to read.</param>
+    /// <returns></returns>
+    public static string[,] ReadCSV(string fileName)
+    {
+        // Open the file
+        StreamReader file = new StreamReader(fileName);
+
+        // Read the header row
+        string[] headers = file.ReadLine().Split(',');
+
+        // Initialize the 2D array
+        string[,] output = new string[headers.Length, headers.Length];
+
+        // Read the file line by line
+        while (!file.EndOfStream)
+        {
+            string[] row = file.ReadLine().Split(',');
+
+            // Populate the 2D array
+            for (int i = 0; i < headers.Length; i++)
+            {
+                output[i, 0] = headers[i];
+                output[i, 1] = row[i];
+            }
+        }
+
+        // Close the file
+        file.Close();
+
+        // Return the 2D array
+        return output;
+    }
+}
